394e4ce5fe10975f76d2fb38273532c3cc42b905
unknown <contato.geanmickael@gmail.com>
2023-07-25 19:26:07 -0300

    v1.1.0a

--- Program.cs
+++ Program.cs
@@ -11,14 +11,19 @@ namespace GeradorDeSenhas
         var Quantidade = 25;
 
         DesenharMenu();
-
-        GetPassword.GetRandomPassword(Alphabet,SpecialCharacters, Quantidade);
-
+        EscreverOpcoes(3,4,Alphabet, SpecialCharacters);
+        
         Console.SetCursorPosition(0,31);
 
       }
+      static void Menu(string alphabet, string specialcharacters)
+      {
+        DesenharMenu();
+        EscreverOpcoes(3,4,alphabet, specialcharacters);
+      }
       static void DesenharMenu()
       {
+        Console.Clear();
         Console.Write("+");
         for(var i=0; i < 48; i++)
         {
@@ -70,6 +75,47 @@ namespace GeradorDeSenhas
             Console.Write("=");
         }
         Console.WriteLine("+");
+      }
+
+      static void EscreverOpcoes(int x, int y, string alphabet, string specialcharacters)
+      {
+        Console.SetCursorPosition(x,y);
+        Console.WriteLine("[1] - Gerar senhas Alfanuméricas");
+        Console.SetCursorPosition(x,y+1);
+        Console.WriteLine("[2] - Gerar senhas Numéricas");
+        Console.SetCursorPosition(x,y+2);
+        Console.WriteLine("[3] - Gerar senhas com caracteres");
+        Console.SetCursorPosition(x,y+3);
+        Console.WriteLine("[4] - Gerar senhas com caracteres especiais");
+        Console.SetCursorPosition(x,y+4);
+        Console.WriteLine("[5] - Sair");
+        Console.SetCursorPosition(x,y+6);
+        Console.WriteLine("Opção:");
+        Console.SetCursorPosition(x+10,y+6);
+        
+        var option_menu = int.Parse(Console.ReadLine());
+
+        switch(option_menu)
+        {
+            case 1:
+            {
+                Console.Clear();
+                DesenharMenu();
+                Console.SetCursorPosition(x,y);
+                Console.WriteLine("Quantas senhas deseja gerar?");
+                Console.SetCursorPosition(x,y+2);
+                Console.WriteLine("Opção:");
+                Console.SetCursorPosition(x+10,y+2);
+                var QtdSenhas = int.Parse(Console.ReadLine());
+                GetPassword.GetRandomPassword(alphabet,specialcharacters,QtdSenhas);
+                Menu(alphabet, specialcharacters);
+                break;
+            }
+            case 2:break;
+            case 3:break;
+            case 4:break;
+            default:Console.SetCursorPosition(x,y+6); Console.WriteLine("Programa finalizado!");break;
+        }
       }
 
     }
