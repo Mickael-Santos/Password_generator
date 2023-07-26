56c175e59a4bab795ae75bdbb8e66e648792ffca
unknown <contato.geanmickael@gmail.com>
2023-07-26 17:37:12 -0300

    v1.2.0a

--- GetPassword.cs
+++ GetPassword.cs
@@ -1,72 +1,171 @@
 
 namespace GeradorDeSenhas
-{ 
-  public class GetPassword
-  {
-    public static void GetRandomPassword(string alphabet, string specialcharacters, int quantidade=0)
+{
+    public class GetPassword
     {
-      if(quantidade < 20)
-      {
-        for(var senhas =0; senhas < quantidade; senhas ++)
+        public static void GetRandomPassword(string alphabet, string specialcharacters, int quantidade = 0)
         {
-            var indexY = senhas + 2;
-            var id = senhas + 1;
-            Console.SetCursorPosition(53,indexY);
-             Console.Write("| ");
-            Console.Write(id);
-            Console.Write("| ");
-            DateTime data = DateTime.Now;
-            Console.Write(data.ToString(System.Globalization.CultureInfo.CreateSpecificCulture("pt-BR")));
-            Console.Write("| ");
-            for(var i=0; i < 4; i++)
+            if (quantidade <= 25)
             {
-                Console.Write(GetRandomChar(alphabet));
+                for (var senhas = 0; senhas < quantidade; senhas++)
+                {
+                    var indexY = senhas + 2;
+                    var id = senhas + 1;
+                    Console.SetCursorPosition(53, indexY);
+                    Console.Write("| ");
+                    Console.Write(id);
+                    Console.Write("| ");
+                    DateTime data = DateTime.Now;
+                    Console.Write(data.ToString(System.Globalization.CultureInfo.CreateSpecificCulture("pt-BR")));
+                    Console.Write("| ");
+                    for (var i = 0; i < 4; i++)
+                    {
+                        Console.Write(GetRandomChar(alphabet));
+                    }
+                    for (var i2 = 0; i2 < 4; i2++)
+                    {
+                        Console.Write(GetRandomNumber(0, 10));
+                    }
+                    Console.Write(GetRandomSpecialChar(specialcharacters));
+                    Console.Write(" |\n");
+
+                }
             }
-            for(var i2=0; i2 < 4; i2++)
+            else
             {
-               Console.Write(GetRandomNumber(0,10));  
-            } 
-            Console.Write(GetRandomSpecialChar(specialcharacters));
-            Console.Write(" |\n");
-            
-        } 
-      }
-      else
-      {
-       
-        Console.SetCursorPosition(54,2);
-        Console.WriteLine("Essa quantidade de senhas");
-        Console.SetCursorPosition(54,3);
-        Console.WriteLine("Não pode ser impressa!");
-      }  
-       
+
+                Console.SetCursorPosition(54, 2);
+                Console.WriteLine("Essa quantidade de senhas");
+                Console.SetCursorPosition(54, 3);
+                Console.WriteLine("Não pode ser impressa!");
+            }
+
+        }
+
+        public static void GetNumberPassword(int quantidade)
+        {
+            if (quantidade <= 25)
+            {
+                for (var senhas = 0; senhas < quantidade; senhas++)
+                {
+                    var indexY = senhas + 2;
+                    var id = senhas + 1;
+                    Console.SetCursorPosition(53, indexY);
+                    Console.Write("| ");
+                    Console.Write(id);
+                    Console.Write("| ");
+                    DateTime data = DateTime.Now;
+                    Console.Write(data.ToString(System.Globalization.CultureInfo.CreateSpecificCulture("pt-BR")));
+                    Console.Write("| ");
+                    for (var i2 = 0; i2 < 8; i2++)
+                    {
+                        Console.Write(GetRandomNumber(0, 10));
+                    }
+                    Console.Write(" |\n");
+
+                }
+            }
+            else
+            {
+
+                Console.SetCursorPosition(54, 2);
+                Console.WriteLine("Essa quantidade de senhas");
+                Console.SetCursorPosition(54, 3);
+                Console.WriteLine("Não pode ser impressa!");
+            }
+        }
+
+        public static void GetCharPassword(string alphabet, int quantidade)
+        {
+            if (quantidade <= 25)
+            {
+                for (var senhas = 0; senhas < quantidade; senhas++)
+                {
+                    var indexY = senhas + 2;
+                    var id = senhas + 1;
+                    Console.SetCursorPosition(53, indexY);
+                    Console.Write("| ");
+                    Console.Write(id);
+                    Console.Write("| ");
+                    DateTime data = DateTime.Now;
+                    Console.Write(data.ToString(System.Globalization.CultureInfo.CreateSpecificCulture("pt-BR")));
+                    Console.Write("| ");
+                    for (var i = 0; i < 8; i++)
+                    {
+                        Console.Write(GetRandomChar(alphabet));
+                    }
+                    Console.Write(" |\n");
+
+                }
+            }
+            else
+            {
+
+                Console.SetCursorPosition(54, 2);
+                Console.WriteLine("Essa quantidade de senhas");
+                Console.SetCursorPosition(54, 3);
+                Console.WriteLine("Não pode ser impressa!");
+            }
+        }
+
+        public static void GetSpecialCharPassword(string specialcharacters, int quantidade)
+        {
+            if (quantidade <= 25)
+            {
+                for (var senhas = 0; senhas < quantidade; senhas++)
+                {
+                    var indexY = senhas + 2;
+                    var id = senhas + 1;
+                    Console.SetCursorPosition(53, indexY);
+                    Console.Write("| ");
+                    Console.Write(id);
+                    Console.Write("| ");
+                    DateTime data = DateTime.Now;
+                    Console.Write(data.ToString(System.Globalization.CultureInfo.CreateSpecificCulture("pt-BR")));
+                    Console.Write("| ");
+                    for (var i = 0; i < 8; i++)
+                    {
+                        Console.Write(GetRandomSpecialChar(specialcharacters));
+                    }
+                    Console.Write(" |\n");
+
+                }
+            }
+            else
+            {
+
+                Console.SetCursorPosition(54, 2);
+                Console.WriteLine("Essa quantidade de senhas");
+                Console.SetCursorPosition(54, 3);
+                Console.WriteLine("Não pode ser impressa!");
+            }
+        }
+
+        private static char GetRandomChar(string s)
+        {
+            Random rand = new Random();
+            var index = rand.Next(0, s.Length);
+            var character = s[index];
+
+            return character;
+        }
+
+        private static char GetRandomSpecialChar(string s)
+        {
+            Random rand = new Random();
+            var index = rand.Next(0, s.Length);
+            var character = s[index];
+
+            return character;
+        }
+
+        private static int GetRandomNumber(int i, int f)
+        {
+            Random rand = new Random();
+            var Random_number = rand.Next(i, f);
+            return Random_number;
+        }
     }
-    
-    private static char GetRandomChar(string s)
-    {
-      Random rand = new Random();
-      var index = rand.Next(0, s.Length);
-      var character = s[index];
-      
-      return character;
-    }
-    
-    private static char GetRandomSpecialChar(string s)
-    {
-      Random rand = new Random();
-      var index = rand.Next(0, s.Length);
-      var character = s[index];
-      
-      return character;
-    }
-    
-    private static int GetRandomNumber(int i, int f)
-    {
-      Random rand = new Random();
-      var Random_number = rand.Next(i, f);
-      return Random_number;
-    }
-  }
 }
-    
+
 
--- Program.cs
+++ Program.cs
@@ -4,119 +4,173 @@ namespace GeradorDeSenhas
 {
     class Gerador
     {
-      static void Main() 
-      {
-        var Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        var SpecialCharacters = "'!@#$%¨&*";
-        var Quantidade = 25;
-
-        DesenharMenu();
-        EscreverOpcoes(3,4,Alphabet, SpecialCharacters);
-        
-        Console.SetCursorPosition(0,31);
-
-      }
-      static void Menu(string alphabet, string specialcharacters)
-      {
-        DesenharMenu();
-        EscreverOpcoes(3,4,alphabet, specialcharacters);
-      }
-      static void DesenharMenu()
-      {
-        Console.Clear();
-        Console.Write("+");
-        for(var i=0; i < 48; i++)
+        static void Main()
         {
-            Console.Write("=");
+            var Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var SpecialCharacters = "'!@#$%¨&*";
+
+            DesenharMenu();
+            EscreverOpcoes(3, 4, Alphabet, SpecialCharacters);
+
+            Console.SetCursorPosition(0, 31);
+
         }
-        Console.Write("+");
-
-        for(var i1 = 0; i1 < 2; i1++){Console.Write(" ");}
-
-        Console.Write("+");
-        for(var i=0; i < 48; i++)
+        static void Menu(string alphabet, string specialcharacters)
         {
-            Console.Write("=");
+            DesenharMenu();
+            EscreverOpcoes(3, 4, alphabet, specialcharacters);
         }
-        Console.WriteLine("+");
-
-        
-        for(var i2=0; i2 < 28; i2++)
+        static void DesenharMenu()
         {
-            Console.Write("|");
-            for(var i3=0; i3 < 48; i3++)
+            Console.Clear();
+            Console.Write("+");
+            for (var i = 0; i < 48; i++)
             {
-                Console.Write(" ");
+                Console.Write("=");
             }
-            Console.Write("|");
+            Console.Write("+");
 
-            for(var i1 = 0; i1 < 2; i1++){Console.Write(" ");}
+            for (var i1 = 0; i1 < 2; i1++) { Console.Write(" "); }
 
-            Console.Write("|");
-            for(var i3=0; i3 < 48; i3++)
+            Console.Write("+");
+            for (var i = 0; i < 48; i++)
             {
-                Console.Write(" ");
+                Console.Write("=");
             }
-            Console.WriteLine("|");
-        }
-        
-        Console.Write("+");
-        for(var i=0; i < 48; i++)
-        {
-            Console.Write("=");
-        }
-        Console.Write("+");
+            Console.WriteLine("+");
 
-        for(var i1 = 0; i1 < 2; i1++){Console.Write(" ");}
 
-        Console.Write("+");
-        for(var i=0; i < 48; i++)
-        {
-            Console.Write("=");
-        }
-        Console.WriteLine("+");
-      }
-
-      static void EscreverOpcoes(int x, int y, string alphabet, string specialcharacters)
-      {
-        Console.SetCursorPosition(x,y);
-        Console.WriteLine("[1] - Gerar senhas Alfanuméricas");
-        Console.SetCursorPosition(x,y+1);
-        Console.WriteLine("[2] - Gerar senhas Numéricas");
-        Console.SetCursorPosition(x,y+2);
-        Console.WriteLine("[3] - Gerar senhas com caracteres");
-        Console.SetCursorPosition(x,y+3);
-        Console.WriteLine("[4] - Gerar senhas com caracteres especiais");
-        Console.SetCursorPosition(x,y+4);
-        Console.WriteLine("[5] - Sair");
-        Console.SetCursorPosition(x,y+6);
-        Console.WriteLine("Opção:");
-        Console.SetCursorPosition(x+10,y+6);
-        
-        var option_menu = int.Parse(Console.ReadLine());
-
-        switch(option_menu)
-        {
-            case 1:
+            for (var i2 = 0; i2 < 28; i2++)
             {
-                Console.Clear();
-                DesenharMenu();
-                Console.SetCursorPosition(x,y);
-                Console.WriteLine("Quantas senhas deseja gerar?");
-                Console.SetCursorPosition(x,y+2);
-                Console.WriteLine("Opção:");
-                Console.SetCursorPosition(x+10,y+2);
-                var QtdSenhas = int.Parse(Console.ReadLine());
-                GetPassword.GetRandomPassword(alphabet,specialcharacters,QtdSenhas);
-                Menu(alphabet, specialcharacters);
-                break;
+                Console.Write("|");
+                for (var i3 = 0; i3 < 48; i3++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|");
+
+                for (var i1 = 0; i1 < 2; i1++) { Console.Write(" "); }
+
+                Console.Write("|");
+                for (var i3 = 0; i3 < 48; i3++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("|");
             }
-            case 2:break;
-            case 3:break;
-            case 4:break;
-            default:Console.SetCursorPosition(x,y+6); Console.WriteLine("Programa finalizado!");break;
+
+            Console.Write("+");
+            for (var i = 0; i < 48; i++)
+            {
+                Console.Write("=");
+            }
+            Console.Write("+");
+
+            for (var i1 = 0; i1 < 2; i1++) { Console.Write(" "); }
+
+            Console.Write("+");
+            for (var i = 0; i < 48; i++)
+            {
+                Console.Write("=");
+            }
+            Console.WriteLine("+");
         }
-      }
+
+        static void EscreverOpcoes(int x, int y, string alphabet, string specialcharacters)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine("[1] - Gerar senhas Alfanuméricas");
+            Console.SetCursorPosition(x, y + 1);
+            Console.WriteLine("[2] - Gerar senhas Numéricas");
+            Console.SetCursorPosition(x, y + 2);
+            Console.WriteLine("[3] - Gerar senhas com caracteres");
+            Console.SetCursorPosition(x, y + 3);
+            Console.WriteLine("[4] - Gerar senhas com caracteres especiais");
+            Console.SetCursorPosition(x, y + 4);
+            Console.WriteLine("[5] - Sair");
+            Console.SetCursorPosition(x, y + 6);
+            Console.WriteLine("Opção:");
+            Console.SetCursorPosition(x + 10, y + 6);
+
+            var option_menu = int.Parse(Console.ReadLine());
+
+            switch (option_menu)
+            {
+                case 1:
+                    {
+                        Console.Clear();
+                        DesenharMenu();
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine("Quantas senhas deseja gerar?");
+                        Console.SetCursorPosition(x, y + 2);
+                        Console.WriteLine("Opção:");
+                        Console.SetCursorPosition(x + 10, y + 2);
+                        var QtdSenhas = int.Parse(Console.ReadLine());
+                        GetPassword.GetRandomPassword(alphabet, specialcharacters, QtdSenhas);
+                        Console.SetCursorPosition(x, y + 4);
+                        Console.WriteLine("Pressione qualquer tecla para continuar...");
+                        Console.SetCursorPosition(x + 44, y + 4);
+                        Console.ReadKey();
+                        Menu(alphabet, specialcharacters);
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.Clear();
+                        DesenharMenu();
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine("Quantas senhas deseja gerar?");
+                        Console.SetCursorPosition(x, y + 2);
+                        Console.WriteLine("Opção:");
+                        Console.SetCursorPosition(x + 10, y + 2);
+                        var QtdSenhas = int.Parse(Console.ReadLine());
+                        GetPassword.GetNumberPassword(QtdSenhas);
+                        Console.SetCursorPosition(x, y + 4);
+                        Console.WriteLine("Pressione qualquer tecla para continuar...");
+                        Console.SetCursorPosition(x + 44, y + 4);
+                        Console.ReadKey();
+                        Menu(alphabet, specialcharacters);
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.Clear();
+                        DesenharMenu();
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine("Quantas senhas deseja gerar?");
+                        Console.SetCursorPosition(x, y + 2);
+                        Console.WriteLine("Opção:");
+                        Console.SetCursorPosition(x + 10, y + 2);
+                        var QtdSenhas = int.Parse(Console.ReadLine());
+                        GetPassword.GetCharPassword(alphabet, QtdSenhas);
+                        Console.SetCursorPosition(x, y + 4);
+                        Console.WriteLine("Pressione qualquer tecla para continuar...");
+                        Console.SetCursorPosition(x + 44, y + 4);
+                        Console.ReadKey();
+                        Menu(alphabet, specialcharacters);
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.Clear();
+                        DesenharMenu();
+                        Console.SetCursorPosition(x, y);
+                        Console.WriteLine("Quantas senhas deseja gerar?");
+                        Console.SetCursorPosition(x, y + 2);
+                        Console.WriteLine("Opção:");
+                        Console.SetCursorPosition(x + 10, y + 2);
+                        var QtdSenhas = int.Parse(Console.ReadLine());
+                        GetPassword.GetSpecialCharPassword(specialcharacters, QtdSenhas);
+                        Console.SetCursorPosition(x, y + 4);
+                        Console.WriteLine("Pressione qualquer tecla para continuar...");
+                        Console.SetCursorPosition(x + 44, y + 4);
+                        Console.ReadKey();
+                        Menu(alphabet, specialcharacters);
+                        break;
+                    }
+                default: Console.SetCursorPosition(x, y + 6); Console.WriteLine("Programa finalizado!"); break;
+            }
+        }
 
     }
 }
